96f3c7fdc72a767838aa68f43bcaafe38446dcc5
ArnoBunckens <arno.bunckens@student.ucll.be>
2021-02-18 12:08:42 +0100

    state working

--- src/components/user/profile.js
+++ src/components/user/profile.js
@@ -1,28 +1,37 @@
 import React, {Component} from 'react';
 import {Link} from "react-router-dom";
+import {connect} from "react-redux";
+
+
+
 
 class profile extends Component {
+
+
     render() {
+        const {email} = this.props
+        console.log(email)
         return (<div className="containter">
             <section>
-            <img src="https://via.placeholder.com/150" alt="Profile Pic" style={imgstyle}/>
-            <section>
-                <p>Gebruikersnaam</p>
-                <div></div>
-                <button style={buttonStyle}>Verander Gebruikersnaam</button>
+                <img src="https://via.placeholder.com/150" alt="Profile Pic" style={imgstyle}/>
+                <section>
+                    <p>{email}</p>
+                    <button style={buttonStyle}>Wijzig Gebruikersnaam</button>
 
 
-            </section>
+                </section>
                 <section>
                     <h4>Jobtitel 1: </h4>
                     <ol>
                         <li>Skill A</li>
                         <li>Skill B</li>
                         <li>Skill C</li>
-                        <button style={buttonStyle}>Verander Job</button>
+                        <button style={buttonStyle}>Wijzig Job</button>
                         <button style={buttonStyle}>Verwijder Job</button>
                     </ol>
-                   <Link to="/addJob"><button style={buttonStyle}>Add job</button></Link>
+                    <Link to="/addJob">
+                        <button style={buttonStyle}>Add job</button>
+                    </Link>
                 </section>
             </section>
 
@@ -39,5 +48,10 @@ const buttonStyle = {
     margin: 7
 }
 
+const mapStateToProps = (state) => {
+    return{
+        email: state.users.email
+    }
+}
 
-export default profile;
+export default connect(mapStateToProps, null) (profile);
