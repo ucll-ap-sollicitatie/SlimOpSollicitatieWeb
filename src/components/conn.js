bb9de191c85fe1a94e25a0d13e4773dcc509a40f
ArnoBunckens <arno.bunckens@student.ucll.be>
2021-02-18 17:01:14 +0100

    Jobs with login

--- src/components/conn.js
+++ src/components/conn.js
@@ -10,8 +10,9 @@ const header = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
     'Access-Control-Allow-Headers': "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
-  }
+}
 
+let jobs = []
 
 const {Pool} = require('pg')
 const pool = new Pool({
@@ -23,8 +24,8 @@ const pool = new Pool({
 })
 
 /**
-let x = "";
-function printName() {
+ let x = "";
+ function printName() {
     pool.query('select * from slimopsol.users', (err, res) => {
         let result = res.rows[0].username
         if (result != null) {
@@ -36,48 +37,49 @@ function printName() {
     return x;
 }
 
-//printName()
+ //printName()
 
-async function log() {
+ async function log() {
     await new Promise(r => setTimeout(r, 2000));
     console.log("TEST= " + x)
 }
 
-//log()
-*/
+ //log()
+ */
 
-function login(email, password){
-    return new Promise((resolve, reject) =>{
+function login(email, password) {
+    return new Promise((resolve, reject) => {
 
-    pool.query('select * from slimopsol.users where email = '+ "'" + email + "'", (err, res) => {
-        console.log(err)
-        if(res.rowCount === 0 ){
-            console.log("foutje")
-            throw new Error("No user found")
-        }
+        pool.query('select * from slimopsol.users where email = ' + "'" + email + "'", (err, res) => {
+            console.log(err)
+            if (res.rowCount === 0) {
+                console.log("foutje")
+                throw new Error("No user found")
+            }
             let uname = res.rows[0].email
             let name = res.rows[0].username
             let pw = res.rows[0].hashedpassword.toString()
             let hashpw = hashCode(password).toString()
             console.log("pw in db: " + pw)
             console.log("Hashed gegeven pw: " + hashpw)
-            if(uname === email && hashpw === pw){
+            if (uname === email && hashpw === pw) {
                 console.log("Ingelogd")
             } else throw new Error("Foute gebruikersnaam/password")
-        const user = {
-            username: name,
-            email: email
-        }
-        resolve(user)
-        return user
-    })
+            const user = {
+                username: name,
+                email: email,
+                jobs: getJobs(email)
+            }
+            resolve(user)
+            return user
+        })
 
-})
+    })
 }
 
-function register(email, password, username){
+function register(email, password, username) {
     let hPassword = hashCode(password);
-    if(!email.includes("@")){
+    if (!email.includes("@")) {
         throw new Error("this is not an email")
     }
     console.log("registering user with email: " + email + " password: " + password + " username: " + username)
@@ -88,15 +90,21 @@ function register(email, password, username){
 
 }
 
+function getJobs(email) {
+    pool.query('select * from slimopsol.jobs where email =' + "'" + email + "'", (err, res) => {
+        jobs =  res.rows
+    })
+    return jobs
+}
+
 function makeJob(email) {
     pool.query('insert into slimopsol.jobs(id, titel, inter, tech, tech2, email) values' + "('" + Math.random() + "', 'Ober','Klantvriendelijkheid', 'Opdienden', 'Bestelling afnemen', " + "'" + email + "')", (err, res) => {
-        console.log(res)
     })
 }
 
 /**
  * Creates server, with possible requests.
- * 
+ *
  * To add a request:
  * add if statement and check on:
  *      - reqUrl.pathname: the path where you will request the api
@@ -106,23 +114,22 @@ function makeJob(email) {
 
 const server = http.createServer((req, res) => makeServer(req, res))
 
-async function makeServer(req, res){
+async function makeServer(req, res) {
     const reqUrl = url.parse(req.url, true);
     if (reqUrl.pathname == "/users/login" && req.method === "POST") {
         let data = '';
         //data will be the ?binary data?
         req.on('data', chunk => {
             data += chunk;
-          })
-          user = {}
+        })
+        user = {}
         //parse data to JSON
-          req.on('end', async () => {
-            jsondata = JSON.parse(data) 
-            try{
+        req.on('end', async () => {
+            jsondata = JSON.parse(data)
+            try {
                 user = await login(jsondata.email, jsondata.pass)
                 console.log(user)
-            }   
-            catch(error){
+            } catch (error) {
                 res.writeHead(200, header);
                 console.log("error" + error)
                 // res.end('NOK: ' + error); 
@@ -130,27 +137,24 @@ async function makeServer(req, res){
             res.writeHead(200, header);
             console.log(JSON.stringify(user))
             res.write(JSON.stringify(user))
-            res.end();   
+            res.end();
         })
-        
-    }
 
-    else if (reqUrl.pathname == "/users/register" && req.method === "POST"){
+    } else if (reqUrl.pathname == "/users/register" && req.method === "POST") {
         let data = '';
         //data will be the ?binary data?
         req.on('data', chunk => {
             data += chunk;
-          })
+        })
         //parse data to JSON
-          req.on('end', () => {
-            jsondata = JSON.parse(data) 
+        req.on('end', () => {
+            jsondata = JSON.parse(data)
             register(jsondata.email, jsondata.pass, jsondata.username)
-        })   
-    
-    }
-    else {
+        })
+
+    } else {
         res.writeHead(200, header);
-        res.end();          
+        res.end();
 
         // res.writeHead(200, {
         //     'Content-Type': 'application/json',
@@ -171,6 +175,7 @@ async function makeServer(req, res){
 
 
 }
+
 /**
  * Create a server that listens on defined port.
  * If there is an error, print it
@@ -182,12 +187,11 @@ server.listen(port, (error) => {
 })
 
 
-
 function hashCode(str) {
     var hash = 0, i, chr;
     for (i = 0; i < str.length; i++) {
-        chr   = str.charCodeAt(i);
-        hash  = ((hash << 5) - hash) + chr;
+        chr = str.charCodeAt(i);
+        hash = ((hash << 5) - hash) + chr;
         hash |= 0;
     }
     return hash;
