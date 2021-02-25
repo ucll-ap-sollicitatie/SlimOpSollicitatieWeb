413ab139b5a7a5643151a80ae73d1738444cd3a9
JensMassie <Jens.massie@student.ucll.be>
2021-02-25 16:22:02 +0100

    most recent on home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState,useEffect  } from 'react';
 import logo from '../logo.svg';
 import '../App.css';
 import {Link} from "react-router-dom";
@@ -9,24 +9,24 @@ import {getRecentVideos} from './user/apiUser'
 
 function Home(props) {
     const history = useHistory();
-
-
-    const vid = "http://localhost:5002/video/" + ""
     return(
     <div className="App">
+        <model-viewer
+        load={recenVid()}>
+
+        </model-viewer>
         <Helmet>
             <title>SOS - Home</title>
         </Helmet>
-
-        <button onClick={recenVisq}>B</button>
         
         <h1>Welkom bij slim op sollicitatie</h1>
 
         <section>
         <h3>Bekijk hier je meest recente videos</h3>
-            <video controls width="360">
-                <source src={vid} type="video/webm"/>
-            </video>
+        <div id="vids">
+
+        </div>
+
         <img src="https://via.placeholder.com/352x240" style={imageStyle}/>
         </section>
         <br/>
@@ -34,6 +34,34 @@ function Home(props) {
         <Link to="/chooseJob"><button>Neem een nieuwe video op</button></Link>
     </div>
     )
+    async function recenVid() {
+        try{
+            var email = props.email
+            var vids = await getRecentVideos(email)
+            const myNode = document.getElementById("vids");
+            myNode.innerHTML = '';
+          
+            var vid1 = document.createElement("video");
+            vid1.controls = true
+            vid1.setAttribute('width',"360")
+            vid1.setAttribute('src', "http://localhost:5002/video/" + vids[0])
+            vid1.setAttribute('type',"video/webm")
+            vid1.setAttribute('id', 'vid1')
+            document.getElementById("vids").append(vid1)
+
+            var vid2 = document.createElement("video");
+            vid2.controls = true
+            vid2.setAttribute('width',"360")
+            vid2.setAttribute('src', "http://localhost:5002/video/" + vids[1])
+            vid2.setAttribute('type',"video/webm")
+            document.getElementById("vids").append(vid2)
+
+        }catch(err){
+            console.log(err)
+        }
+        return vids 
+    }
+    
 }
 
 const imageStyle= {
@@ -43,22 +71,13 @@ const imageStyle1= {
     marginRight: 100,
 }
 
-async function recenVisq(email) {
-    try{
-    var vids = await getRecentVideos("t@t")
-    console.log("recenVisq: " + vids)
-    }catch(err){
-        console.log(err)
-    }
-    return vids 
-}
 
 
 const mapStateToProps = (state) => {
     console.log("state")
     console.log(state)
     return{
-        email: state.users
+        email: state.users.email
     }
 }
 
