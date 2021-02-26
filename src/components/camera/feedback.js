867437895c5b07e6d3c8a6fa3243dd15d5937c70
JensMassie <Jens.massie@student.ucll.be>
2021-02-26 12:08:03 +0100

    feedback per question

--- src/components/camera/feedback.js
+++ src/components/camera/feedback.js
@@ -1,12 +1,14 @@
 import { Helmet } from 'react-helmet';
 import {connect} from 'react-redux'
 import React, { useState } from 'react';
-
+import {getAllFeedbackArray} from '../questions/feedbackquestions'
 function Feedback(props){
 
-    const criteria = ["Criteria A", "Criteria B", "Criteria C", "Criteria D"]
+    const criteria = getAllFeedbackArray()
 
     const [score, setScore] = useState('');
+    const [criterium, setCrit] = useState(criteria[0])
+
     var scor = 0;
     var scores = {}
 
@@ -14,9 +16,9 @@ function Feedback(props){
         criteria.forEach(el => {
             scores[el] = 0
         });
+        console.log(scores)
     }
     console.log(scores)
-
     return(
         <div className="App">
             <Helmet>
@@ -28,7 +30,7 @@ function Feedback(props){
                 <video id="video" src={"http://localhost:5002/video/" + props.selectedvid} controls></video>
                 
 
-                {criteria.map(criterium => 
+                {/* {criteria.map(criterium => 
                     {
                         return(
                         <section>
@@ -42,10 +44,24 @@ function Feedback(props){
                             <button id={criterium + "ZG"} onClick={ZGClick}>Zeer goed</button>
                         </section>)
                     }    
-                )}
+                )} */}
+                <section>
+                    <p>{criterium}</p>
+                    <button id={criterium + "ZS"} onClick={ZSClick}>Zeer slecht</button>
+                    <button id={criterium} onClick={OKClick}>OK</button>
+                    <div id={criterium + "ok"} style={{display: "none"}}>
+                        <label for={criterium + "score"}>Points (between 2/5 and 4/5):</label>
+                        <input type="range" min="2" max="4" class="slider" id={criterium + "score"} onChange={updateSliderScore}/>
+                    </div>
+                    <button id={criterium + "ZG"} onClick={ZGClick}>Zeer goed</button>
+
+                </section>
+
+                <button onClick={changeCritprev} >prev</button>
+                <button onClick={changeCritnext}>next</button>
 
                 <button onClick={calcScore}>Calculate score</button>
-                <button onClick={resetScore}>Reset score</button>
+                <button onClick={createEmptyMap}>Reset score</button>
                 <p>{score}</p>
 
             </section>
@@ -53,28 +69,34 @@ function Feedback(props){
         
     )
 
-    function resetScore(){
-        createEmptyMap()
-        criteria.forEach(crit => {
-            var elem = document.getElementById(crit + "ZS")
-            elem.style.display = "";
-            var elem = document.getElementById(crit + "ZG")
-            elem.style.display = "";
-            var elem = document.getElementById(crit)
-            elem.style.display = "";
+    function changeCritprev() {
+        var index = criteria.indexOf(criterium)
+        if (index > 0){
+            index = index - 1
+        }
+        else{
+            index = criteria.length -1
+        }
+        setCrit(criteria[index])
+    }
 
-            var elem = document.getElementById(crit + "ok")
-            elem.style.display = "none";
-        })
-        setScore(0)
+    function changeCritnext() {
+        var index = criteria.indexOf(criterium)
+        if (index < criteria.length -1){
+            index = index + 1
+        }
+        else{
+            index = 0
+        }
+        setCrit(criteria[index])
+    }
+
+    function resetScore(){
+        
     }
 
     function calcScore(){
-        var sum = 0
-        Object.keys(scores).forEach(el => {
-            sum += scores[el]
-        })
-        setScore(sum)
+        
     }
 
     function updateSliderScore(e){
@@ -91,32 +113,12 @@ function Feedback(props){
         var id = e.target.id
         id = id.split("ZG")[0]
         scores[id] = 5
-
-        //set elements invis
-        //Zeer slecht
-        var elem = document.getElementById(id + "ZS")
-        elem.style.display = "none";
-
-        //OK
-        var elem = document.getElementById(id)
-        elem.style.display = "none";
         console.log(scores)
-
     }
 
     function ZSClick(e){
         var id = e.target.id
         id = id.split("ZS")[0]
-
-        //set elements invis
-        //Zeer goed
-        var elem = document.getElementById(id + "ZG")
-        elem.style.display = "none";
-        //OK
-        var elem = document.getElementById(id)
-        elem.style.display = "none";
-        scores[id] = 1
-        console.log(scores)
 
     }
 
@@ -124,19 +126,7 @@ function Feedback(props){
     function OKClick(e){
         var okval = e.target.id
 
-        //set elements invis
 
-        var elem = document.getElementById(okval + "ok")
-        elem.style.display = "block";
-
-        var elem = document.getElementById(okval + "ZG")
-        elem.style.display = "none";
-
-        var elem = document.getElementById(okval + "ZS")
-        elem.style.display = "none";
-
-        var elem = document.getElementById(okval)
-        elem.style.display = "none";
     }
     
     
---
+++ src/components/questions/feedbackquestions.js
@@ -0,0 +1,28 @@
+const fTaken = "Weet -VNAAM- vlot te vertellen welke taken hij moet uitvoeren?"
+const fCarriere = "Is de ervaring die -VNAAM- vertelt echt relevant voor deze job?"
+const fCompetenties = "Hoe geloofwaarig en helder komt in het verhaal naar voor dat -VNAAM- de competentie bezit?"
+const fAttitudes = "We horen een voorbeeld dat authentiek aangeeft waarom -VNAAM- gemotiveerd is voor deze job. "
+const fUitzoeken = "De vraag die -VNAAM- stelde ging die over: \n Het goed kunnen uitvoeren van het werk : zeer goed 5 of 4 \n De verloning voor de job: zeer slecht 1 \n Een vraag naar het selectieproces dat hij/zij dient te doorlopen: OK 2 of 3"
+const fScorenAlgemeen = "Het is niet de hoogte van de score, maar de uitleg die -VNAAM- gaf voor zijn score die je beoordeelt op geloofwaardigheid"
+const fScorenL = " -VNAAM- heeft voor de verkeerde job gesolliciteerd. Hoe goed wordt deze vergissing toegegeven?"
+const fScorenM = "-VNAAM- vertelt waarom hij/zij zich in staat acht om deze job te doen. Maar heeft -VNAAM- ook verteld waarin hij/zij tekortschiet?"
+const fScorenH = "Dan is dit omdat -VNAAM- zich heel goed in staat acht om deze job te doen. Hoe geloofwaardig is zijn uitleg?"
+
+function getAllFeedbackObject(){
+    return{ 
+        "fTaken": fTaken,
+        "fCarriere" : fCarriere,
+        "fCompetenties" : fCompetenties,
+        "fAttitudes" : fAttitudes,
+        "fUitzoeken" : fUitzoeken,
+        "fScorenAlgemeen" : fScorenAlgemeen,
+        "fScorenL" : fScorenL,
+        "fScorenM" : fScorenM,
+        "fScorenH" : fScorenH
+    }
+}
+
+function getAllFeedbackArray(){
+    return [fTaken, fCarriere, fCompetenties, fUitzoeken, fScorenAlgemeen]
+}
+export {getAllFeedbackObject, getAllFeedbackArray};
