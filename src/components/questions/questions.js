094f043221a0d52dec16fea2779719a47d4e969a
JensMassie <Jens.massie@student.ucll.be>
2021-02-27 14:54:12 +0100

    updated questions

--- src/components/questions/questions.js
+++ src/components/questions/questions.js
@@ -2,9 +2,9 @@
  *  -JOB- wordt vervangen door de job titel
  *  -COMP- wordt vervangen door elke competentie
  */
-const vrTaken = "Welke taken denk jij dat het belangrijkste zijn in deze job van -JOB-?"
+const vrTaken = "Je solliciteert voor de job van -JOB-. Vertel eens, welke taken denk jij dat de belangrijkste zijn in deze job?"
 const vrTakenD2 = "Van de eerder genoemde taken, kan je aangeven welke je leuk, lastig of tweeledig vindt? Tweeledig betekent dat je een stukje leuk en een stukje lastig vindt."
-const vrCar = "Welke ervaring heb jij die relevant is om deze taken uit te voeren?"
+const vrCar = "Welke ervaring heb jij die relevant is voor deze job?"
 const vrCom = "Je hebt aangegeven dat competentie -COMP- belangrijk is. Geef een voorbeeld van een situatie waarin je deze competentie hebt toegepast."
 const vrA = "Wat is jouw motivatie voor deze job als -JOB-?"
 const vrU = "Welke vragen heb jij over deze job of over het werken bij ons?"
