45abdcc60b0d31f8d57ac96c26453d16a84c3d94
Jonathan Peypops <jonathan.peypops@student.ucll.be>
2021-03-01 11:11:06 +0100

    time parse

--- src/components/camera/camera.js
+++ src/components/camera/camera.js
@@ -124,7 +124,7 @@ const WebcamStreamCapture = () => {
             var currentdate = new Date()
             var datetime = currentdate.getDate() + "/"
                 + (currentdate.getMonth()+1)  + "/" 
-                + currentdate.getFullYear() + " @ "  
+                + currentdate.getFullYear() + " "  
                 + currentdate.getHours() + ":"  
                 + currentdate.getMinutes() + ":" 
                 + currentdate.getSeconds();
