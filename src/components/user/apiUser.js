dff095290c3b6c4c6f763a2e81a33d9876f0821c
ArnoBunckens <arno.bunckens@student.ucll.be>
2021-03-02 17:52:35 +0100

    register change

--- src/components/user/apiUser.js
+++ src/components/user/apiUser.js
@@ -34,27 +34,27 @@ async function logindb(eml, pss) {
     //return promise so that in login.js the function correctly waits for a response
     return new Promise((resolve, reject) => {
 
-        var data = JSON.stringify({"email": eml, "pass": pss});
-        var config = {
-            method: 'post',
-            url: `${webIp}/users/login`,
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            data: data
-        };
-        axios(config)
-            .then(function (response) {
-                //console.log(response.data)
-                resolve(response.data)
-                return response;
-            })
+        axios
+            // sends post request to the api on this path
+            .post(`https://127.0.0.1:3001/users/login`,
+                //the request body:
+                {
+                    email: eml,
+                    pass: pss,
+                },
+                //without header, it will not send a POST but a OPTIONS request
+                {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    }
+                }
+            ).then(function (response) {
+            //console.log("respData: " + response.data)
+            resolve(response.data)
+            return response.data;
+        })
             .catch(function (error) {
-                console.log("error")
-                resolve(error)
-
                 return "NOK"
-                // console.log(error);
             });
     })
 }
