9c2324e22e0eb3ce8254c22dcace1936e872b4ca
JensMassie <Jens.massie@student.ucll.be>
2021-03-03 11:06:49 +0100

    hide ok

--- src/components/camera/feedback.js
+++ src/components/camera/feedback.js
@@ -63,6 +63,7 @@ function Feedback(props){
     function keepZG(crit){
         document.getElementById(crit + "ZS").style.display = "none"
         document.getElementById(crit).style.display = "none"
+        document.getElementById(crit + "ok").style.display = "none"
         document.getElementById(crit + "ZG").style.display = "inline"
     }
 
@@ -72,6 +73,8 @@ function Feedback(props){
         try{
           document.getElementById(crit + "ZG").style.display = "none"
           document.getElementById(crit).style.display = "none"
+          document.getElementById(crit + "ok").style.display = "none"
+
           document.getElementById(crit + "ZS").style.display = "inline"
 
         }catch(err){
